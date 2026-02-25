b4da7420f06df5e99c8dfff9ede9711dd67d997d
vansh-verma-dev <vanshverma6065@gmail.com>
2026-02-26 00:00:05 +0530

    get data in localstorage for login page

--- loginpage/main.js
+++ loginpage/main.js
@@ -1,5 +1,8 @@
+
+// --------Signup page logic----
 document.querySelector(".accountBtn").addEventListener("click", (e) => {
   e.preventDefault();
+
 
   const username = document.getElementById("name-input").value.trim();
   const surname = document.getElementById("Surname-input").value.trim();
@@ -22,15 +25,43 @@ document.querySelector(".accountBtn").addEventListener("click", (e) => {
     return;
   }
 
+  //--create user data store in  object
   let user = {
     username: username,
     surname: surname,
     email: email,
     password: password,
   };
+  //add date store in localstorage
   localStorage.setItem("user", JSON.stringify(user));
+
   msg.innerHTML="account create succesfull"
-  msg.style.backgroundColor='green';
+  msg.style.backgroundColor='green';  
+});
+
+//------signin page logic---
+document.querySelector('.accountBtn').addEventListener('click',(e)=>{
+  e.preventDefault();
+ 
+  const email = document.getElementById("email-input").value.trim();
+  const password = document.getElementById("password").value.trim();
+  const msg = document.querySelector(".msg");
+
+  if(email ==="" || password ===""){
+ msg.innerHTML = "please fill all requird fields"
+ msg.style.backgroundColor='red'
+  }
+  setInterval(()=>{
+    msg.style.color='transparent'
+    msg.style.backgroundColor='transparent';
+  },3000)
+
+  //get data for localstorage
+  let getdata = JSON.parse(localStorage.getItem("user"));
 
   
-});
+  if(email === getdata.email && password=== getdata.password){
+    msg.innerHTML='login succesfull'
+  }
+
+})
