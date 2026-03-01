ec4cf06fccd2d4d7405876a7facfccc28801ff30
vansh-verma-dev <vanshverma6065@gmail.com>
2026-03-01 23:24:11 +0530

    initial add welcome page

---
+++ rock-paper-scissors/main.js
@@ -0,0 +1,10 @@
+
+document.getElementById('continueBtn').addEventListener('click',()=>{
+ const GamePage = document.querySelector('.game-page'); 
+ const WelcomePage = document.querySelector('.welcome-page');
+GamePage.style.display='block'
+WelcomePage.style.display="none"
+})
+ 
+   
+ 
