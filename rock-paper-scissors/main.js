cdffbe0b06cb63afad44f657dc4c6e233ad312b3
vansh-verma-dev <vanshverma6065@gmail.com>
2026-03-02 01:19:15 +0530

    add update count in click any btn

--- rock-paper-scissors/main.js
+++ rock-paper-scissors/main.js
@@ -1,8 +1,17 @@
 document.getElementById("continueBtn").addEventListener("click", () => {
-
   const GamePage = document.querySelector(".game-page");
   const WelcomePage = document.querySelector(".welcome-page");
 
-  GamePage.style.display = "block";
+  GamePage.style.display = "flex";
   WelcomePage.style.display = "none";
+});
+count = 0;
+const Countbtn = document.querySelectorAll(".Countbtn");
+Countbtn.forEach((btn)=>{
+btn.addEventListener("click", () => {
+  let countvalue = document.getElementById("Count");
+  count++;
+  countvalue.innerText = count;
+})
+
 });
