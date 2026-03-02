2c70ecaa9206c1c0a75cd0b02ad5a02e5a2ad0fe
vansh-verma-dev <vanshverma6065@gmail.com>
2026-03-02 09:56:49 +0530

    display user and AI choices in Screen

--- rock-paper-scissors/main.js
+++ rock-paper-scissors/main.js
@@ -1,3 +1,5 @@
+
+//welcome page & game-page toggle
 document.getElementById("continueBtn").addEventListener("click", () => {
   const GamePage = document.querySelector(".game-page");
   const WelcomePage = document.querySelector(".welcome-page");
@@ -5,13 +7,45 @@ document.getElementById("continueBtn").addEventListener("click", () => {
   GamePage.style.display = "flex";
   WelcomePage.style.display = "none";
 });
+
+//All btn click add Totla count 
 count = 0;
+
 const Countbtn = document.querySelectorAll(".Countbtn");
 Countbtn.forEach((btn)=>{
 btn.addEventListener("click", () => {
   let countvalue = document.getElementById("Count");
+
   count++;
   countvalue.innerText = count;
 })
-
 });
+
+//User Choice Btn 
+Countbtn.forEach((btn)=>{
+  btn.addEventListener('click',()=>{
+    const UserChoice = btn.innerText.toLowerCase();
+    playGame(UserChoice);
+  })
+
+})
+
+const UserSelect = document.querySelector('.UserSelect');
+const Aiselect = document.querySelector('.Aiselect');
+// print user choice 
+function playGame(Choice){
+console.log("User :", Choice);
+UserSelect.innerHTML = "You Choice:  " + Choice;
+  Aiselect.innerHTML = "Ai Choice: "+Aigenrate();
+ 
+}
+
+// choice Ai 
+
+function Aigenrate(){
+  let AiChoices = ['rock', 'paper' , 'scissor']
+  let randamChoices = Math.floor(Math.random() *3);
+   return  AiChoices[randamChoices];
+}
+
+
